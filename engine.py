9056d402bb0d87fb9b9c49111fe7c7bcad692d23
TApplesmith <sgf3211@gmail.com>
2019-01-05 10:55:51 -0600

    Moved engine constants

--- engine.py
+++ engine.py
@@ -8,68 +8,37 @@ from fov_functions import initialize_fov, recompute_fov
 from game_messages import Message, MessageLog
 from game_states import GameStates
 from input_handlers import handle_keys, handle_mouse
+from loader_functions.initialize_new_game import get_constants
 from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all, RenderOrder
 
+
 def main():
-    screen_width = 80
-    screen_height = 50
-
-    bar_width = 20
-    panel_height = 7
-    panel_y = screen_height - panel_height
-
-    message_x = bar_width + 2
-    message_width = screen_width - bar_width - 2
-    message_height = panel_height - 1
-
-    # map size
-    map_width = 80
-    map_height = 43
-
-    # map generation parameters
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    # field of view parameters
-    fov_algorithm = 0 # I don't like 1,3
-    fov_light_walls = True
-    fov_radius = 10
-
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
-    #testing vars
-    max_items_per_room = 5
-
-    colors = {
-        'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150),
-        'light_wall': libtcod.Color(130, 110, 50),
-        'light_ground': libtcod.Color(200, 180, 50)
-    }
-
+    constants = get_constants()
+    
     fighter_component = Fighter(hp=30, defense=2, power=5)
     inventory_component = Inventory(26)
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, inventory=inventory_component)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
+                    fighter=fighter_component, inventory=inventory_component)
     entities = [player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(constants['screen_width'], constants['screen_height'], constants['window_title'], False)
 
-    con = libtcod.console_new(screen_width, screen_height)
-    panel = libtcod.console_new(screen_width, panel_height)
+    con = libtcod.console_new(constants['screen_width'], constants['screen_height'])
+    panel = libtcod.console_new(constants['screen_width'], constants['panel_height'])
 
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room, max_items_per_room)
+    game_map = GameMap(constants['map_width'], constants['map_height'])
+    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'],
+                      constants['map_width'], constants['map_height'], player, entities,
+                      constants['max_monsters_per_room'], constants['max_items_per_room'])
 
     fov_recompute = True
 
     fov_map = initialize_fov(game_map)
 
-    message_log = MessageLog(message_x, message_width, message_height)
+    message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -78,22 +47,24 @@ def main():
     previous_game_state = game_state
 
     targeting_item = None
-    
+
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
         if fov_recompute:
-            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
-        
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
-                       screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
+            recompute_fov(fov_map, player.x, player.y, constants['fov_radius'], constants['fov_light_walls'],
+                          constants['fov_algorithm'])
+
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log,
+                   constants['screen_width'], constants['screen_height'], constants['bar_width'],
+                    constants['panel_height'], constants['panel_y'], mouse, constants['colors'], game_state)
+
         fov_recompute = False
-        
+
         libtcod.console_flush()
 
         clear_all(con, entities)
 
-        # Action handlers
         action = handle_keys(key, game_state)
         mouse_action = handle_mouse(mouse)
 
@@ -115,7 +86,6 @@ def main():
             destination_x = player.x + dx
             destination_y = player.y + dy
 
-            
             if not game_map.is_blocked(destination_x, destination_y):
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
@@ -123,7 +93,7 @@ def main():
                     attack_results = player.fighter.attack(target)
                     player_turn_results.extend(attack_results)
                 else:
-                    player.move(dx,dy)
+                    player.move(dx, dy)
 
                     fov_recompute = True
 
@@ -136,7 +106,6 @@ def main():
                     player_turn_results.extend(pickup_results)
 
                     break
-
             else:
                 message_log.add_message(Message('There is nothing here to pick up.', libtcod.yellow))
 
@@ -159,9 +128,10 @@ def main():
 
         if game_state == GameStates.TARGETING:
             if left_click:
-                target_x, target_y, = left_click
+                target_x, target_y = left_click
 
-                item_use_results = player.inventory.use(targeting_item, entities=entities, fov_map=fov_map, target_x=target_x, target_y=target_y)
+                item_use_results = player.inventory.use(targeting_item, entities=entities, fov_map=fov_map,
+                                                        target_x=target_x, target_y=target_y)
                 player_turn_results.extend(item_use_results)
             elif right_click:
                 player_turn_results.append({'targeting_cancelled': True})
@@ -205,6 +175,11 @@ def main():
             if item_consumed:
                 game_state = GameStates.ENEMY_TURN
 
+            if item_dropped:
+                entities.append(item_dropped)
+
+                game_state = GameStates.ENEMY_TURN
+
             if targeting:
                 previous_game_state = GameStates.PLAYERS_TURN
                 game_state = GameStates.TARGETING
@@ -218,11 +193,6 @@ def main():
 
                 message_log.add_message(Message('Targeting cancelled'))
 
-            if item_dropped:
-                entities.append(item_dropped)
-
-                game_state = GameStates.ENEMY_TURN
-            
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
                 if entity.ai:
@@ -250,6 +220,7 @@ def main():
                         break
             else:
                 game_state = GameStates.PLAYERS_TURN
+
 
 if __name__ == '__main__':
     main()
